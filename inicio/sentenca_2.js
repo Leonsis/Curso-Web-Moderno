467dc12c618d7c204ca3da706d411e56108dd5a1
Leonsis <caiolenni@gmail.com>
2025-10-17 19:28:03 -0300

    Enviando mais uma aula, sentenca_2.js, onde está sendo explicado o console.log() e um pouco mais sobre as sentenças

---
+++ inicio/sentenca_2.js
@@ -0,0 +1,14 @@
+console.log(1 + 3);
+/* Com o console.log(), podemos imprimir mensagens no console do navegador 
+ * ou do ambiente de execução (como Node.js).
+*/
+
+console.log("Olá, mundo!");
+/* Para imprimir texto, usamos aspas simples (' ') ou aspas duplas (" "). 
+
+ * No caso um algoritimo é umsa sequencia de sentenças que descrevem um processo.
+ * Uma coisa importante nisso é que a ordem das sentenças importa.
+*/
+console.log("Paasso 1: Iniciar o processo.");
+console.log("Passo 2: Executar o processo.");
+console.log("Passo 3: Finalizar o processo.");
