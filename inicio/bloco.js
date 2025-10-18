2b0e6ca3e1babafc4bb60f2194a5d4a57e607feb
Leonsis <caiolenni@gmail.com>
2025-10-18 08:58:42 -0300

    Enviando mais uma aula, bloco.js. Explicando como é um bloco de código.

---
+++ inicio/bloco.js
@@ -0,0 +1,21 @@
+/* Um bloco em JavaScript é uma seção de código delimitada por chaves `{}`.
+ * Ele pode conter declarações, expressões e outras estruturas de controle.
+ * Blocos são frequentemente usados em funções, loops e condicionais para
+ agrupar múltiplas instruções que devem ser executadas juntas.
+ */
+
+{
+    /* Exemplo de um bloco.
+     * Tambem é possivel criar um bloco de código dentro de outro bloco.*/
+    console.log("Passo 1"); // Sentença 1
+    console.log("Passo 2"); // Sentença 2
+    console.log("Passo 3"); // Sentença 3
+}
+
+{
+    console.log("Passo 4"); // Sentença 4
+    { // Bloco aninhado
+        console.log("Passo 5"); // Sentença 5
+        console.log("Passo 6"); // Sentença 6
+    }
+}
