efb44f6a16725174acf5a462c079f1171b634656
Leonsis <caiolenni@gmail.com>
2025-10-18 15:07:46 -0300

    Dessa vez endiando o primeiro exercicio do curso. Desadio_senteca_bloco.js. No Diretorio Resposta-Desafio que está na rais do projeto

---
+++ Resposta-Desafios/desafio_sentenca_bloco.js
@@ -0,0 +1,25 @@
+/* 
+    Crie sentenças de código que exibam os números ímpares e os números pares.
+    Divida-os em dois blocos.
+    Exemplo:
+    Bloco 1 - números ímpares
+    Bloco 2 - números pares
+*/
+
+{ // Bloco de numero Ímpares
+    console.log("Numeros ímpares: ");
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{ // Boco de numeros Pares
+    console.log("Numeros Pares: ");
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,7 @@
+/* 
+    Crie sentenças de código que exibam os números ímpares e os números pares.
+    Divida-os em dois blocos.
+    Exemplo:
+    Bloco 1 - números ímpares
+    Bloco 2 - números pares
+*/
