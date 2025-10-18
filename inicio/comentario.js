f7f65119aee2c157e08f4e0fc3bc5b9c23441cb3
Leonsis <caiolenni@gmail.com>
2025-10-18 08:45:54 -0300

    Enviando mais uma aula, comentarios.js, onde é explicado os tipos de código e como podem ser usados.

---
+++ inicio/comentario.js
@@ -0,0 +1,12 @@
+/*O comentariao serve para explicar o codigo, documentar ou desativar partes do codigo.
+ * Ele é ignorado pelo interpretador.*/
+
+// O console.log() imprime mensagens no console.
+console.log();
+console.log("testet"); // Mais um exemplo de console.log().
+// console.log(12345);
+
+// Com o // podemos fazer comentários de uma única linha.
+
+/* Com o /* e * / (sem o espaço) podemos fazer comentários de múltiplas linhas.
+   Eles são úteis para explicar partes mais complexas do código ou para desativar blocos inteiros de código. */
